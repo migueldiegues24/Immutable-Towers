ff68ad87df45ae2eea3e8b8d3f01a1ff2ad2078b
Miguel <a107361@alunos.uminho.pt>
2025-01-08 19:39:11 +0000

    Alterações e Tarefa2 Completa

--- lib/Tarefa2.hs
+++ lib/Tarefa2.hs
@@ -10,43 +10,73 @@ Módulo para a realização da Tarefa 2 de LI1 em 2024/25.
 module Tarefa2 where
 
 import LI12425
+import Data.List
 
 
 -- 1
 -- Função que calcula a lista de inimigos no alcance da torre
 
 inimigosNoAlcance :: Torre -> [Inimigo] -> [Inimigo]
-inimigosNoAlcance _ [] = []
-inimigosNoAlcance torre (h:t) =
-    let
-        posTorre = posicaoTorre torre
-        posInimigo = posicaoInimigo h
-        rangeTorre = alcanceTorre torre
-    in
-        if distanciaEntreDoisPontos posTorre posInimigo <= rangeTorre then h : inimigosNoAlcance torre t else inimigosNoAlcance torre t
-
--- Função auxiliar para calcular a distância entre dois pontos
-
-distanciaEntreDoisPontos :: Posicao -> Posicao -> Distancia
-distanciaEntreDoisPontos (x1,y1) (x2,y2) = sqrt $ (x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2)
+inimigosNoAlcance torre inimigos = filter (\inimigo -> distancia (posicaoTorre torre) (posicaoInimigo inimigo) <= alcanceTorre torre) inimigos
+  where
+    distancia (x1,y1) (x2,y2) = sqrt $ (x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2)
 
 
 -- 2
 -- Função que atualiza o inimigo depois deste ser atingido pela torre
 
 atingeInimigo :: Torre -> Inimigo -> Inimigo
-atingeInimigo = undefined
+atingeInimigo torre inimigo = inimigo { 
+  vidaInimigo = max 0 (vidaInimigo inimigo - danoTorre torre),
+  projeteisInimigo = atualizaProjeteis (projetilTorre torre) (projeteisInimigo inimigo)
+}
 
 
--- Função auxiliar que atualiza a vida do Inimigo depois do mesmo ser atingido pela torre
 
-atualizaVidaInimigo :: Torre -> Inimigo -> Inimigo
-atualizaVidaInimigo torre inimigo =
-    let
-        dano = danoTorre torre
-        novaVida = max 0 (vidaInimigo inimigo - dano)
-    in
-        inimigo { vidaInimigo = novaVida }
+-- Funções auxiliares
+
+atualizaProjeteis :: Projetil -> [Projetil] -> [Projetil]
+atualizaProjeteis novoProjetil projeteisAtuais =
+  case tipoProjetil novoProjetil of
+    Fogo   -> atualizaFogo novoProjetil projeteisAtuais
+    Gelo   -> atualizaGelo novoProjetil projeteisAtuais
+    Resina -> atualizaResina novoProjetil projeteisAtuais
+
+
+atualizaFogo :: Projetil -> [Projetil] -> [Projetil]
+atualizaFogo novo projeteis =
+  let
+    projeteisSemGelo = filter (\p -> tipoProjetil p /= Gelo) projeteis
+  in
+    case find (\p -> tipoProjetil p == Resina) projeteisSemGelo of
+      Just _  -> somaOuDobra Fogo novo projeteisSemGelo True -- Dobra duração do Fogo
+      Nothing -> somaOuDobra Fogo novo projeteisSemGelo False
+
+
+atualizaGelo :: Projetil -> [Projetil] -> [Projetil]
+atualizaGelo novo projeteis =
+  let
+    projeteisSemFogo = filter (\p -> tipoProjetil p /= Fogo) projeteis
+  in
+    somaOuDobra Gelo novo projeteisSemFogo False
+
+
+atualizaResina :: Projetil -> [Projetil] -> [Projetil]
+atualizaResina novo projeteis =
+  somaOuDobra Resina novo projeteis False
+
+
+somaOuDobra :: TipoProjetil -> Projetil -> [Projetil] -> Bool -> [Projetil]
+somaOuDobra tipo novo projeteis dobra =
+  let
+    duracaoNovo = case duracaoProjetil novo of
+                    Finita t -> t
+                    Infinita -> 0  -- Projéteis infinitos não somam duração
+    (restantes, existentes) = partition (\p -> tipoProjetil p == tipo) projeteis
+    duracaoExistente = sum [t | Projetil _ (Finita t) <- existentes]
+    novaDuracao = if dobra then 2 * duracaoNovo else duracaoNovo + duracaoExistente
+  in
+    Projetil tipo (Finita novaDuracao) : restantes
 
 
 -- 3
